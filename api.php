cf339a51ef0e889fda739300f62496c564e99344
Sergio Alcala <checo_2k2@hotmail.com>
2014-01-06 17:55:46 -0600

    Acomodo de archivos Listo para su Uso

--- api.php
+++ api.php
@@ -2,41 +2,86 @@
 
 require('twitter/twitteroauth.php'); // Ruta donde tenemos en nuestro servidor la librería de Abraham.
   
-  define('_CONSUMER_KEY','GSjEfv0ojs3Kj8TZVsQuw'); // Sustituye el segundo parámetro por tu Consumer key.
-  define('_CONSUMER_SECRET','X8HDTUTENr7CzPkcv01WEQw5uSOvadzLvcN00eiJg'); // Sustituye el segundo parámetro por tu Consumer secret.
-  define('_OAUTH_TOKEN','739529196-e1wOYlV0ddLrizN4ULQr5b9BAUPEACFJ21YI9QzT'); // Sustituye el segundo parámetro por tu OAuth Token.
-  define('_OAUTH_TOKEN_SECRET','17rSBLI42c67nHqDKEt9tuykuffCVsjraFK0bKF2Bulia'); // Sustituye el segundo parámetro por tu OAuth Token Secret.
+  define('_CONSUMER_KEY','8FyBUlHFwm7Trketnleg'); // Sustituye el segundo parámetro por tu Consumer key.
+  define('_CONSUMER_SECRET','KCMxPhKj2r2jitPB0WpVz0Ij1LGGm1MXPmlThTEJ34'); // Sustituye el segundo parámetro por tu Consumer secret.
+  define('_OAUTH_TOKEN','2274936284-yOwD67gG0H8TdlWIDC7SB6hgbO2R33hFRy9fIYn'); // Sustituye el segundo parámetro por tu OAuth Token.
+  define('_OAUTH_TOKEN_SECRET','5vWewFS7qN7Cl2FcEzqbL9Az8GF70ESRG3FbxHhwvZdQO'); // Sustituye el segundo parámetro por tu OAuth Token Secret.
 
   function getConnectionWithAccessToken() {
     $connection = new TwitterOAuth(_CONSUMER_KEY, _CONSUMER_SECRET,_OAUTH_TOKEN, _OAUTH_TOKEN_SECRET);
     return $connection;
   }
-
+  
   //$twitter = $connection->post('statuses/update', array('status' => $noticia));  //Sirve para publicar en twitter
   //$twitter = $connection->post('direct_messages/new', array('screen_name' => $nombre,'text' => $noticia)); //Enviar mensaje Privado
-
   $connection = getConnectionWithAccessToken();
-  $twitter = $connection->get('direct_messages');
-
-  //echo json_encode($twitter);
-
   $resultado = array();
   $general = array();
-  $i = 0;
+  $accion = $_REQUEST['funcion'];
 
-  foreach ($twitter as $tweet) {
+  if ($accion == 'mensajes') {
+
+      $twitter = $connection->get('direct_messages');
+
+      //echo json_encode($twitter);
+      $i = 0;
+
+      foreach ($twitter as $tweet) {
+        
+        $resultado['id'] = $tweet->id;
+        $resultado['texto'] = $tweet->text;
+        $resultado['de'] = $tweet->sender->screen_name;
+        $resultado['avatar'] = $tweet->sender->profile_image_url;
+        $resultado['fecha'] = $tweet->created_at;
+
+        $general[$i] = $resultado;
+        $i++;
+
+      }
+
+      echo json_encode($general);
     
-    $resultado['id'] = $tweet->id;
-    $resultado['texto'] = $tweet->text;
-    $resultado['de'] = $tweet->sender->screen_name;
-    $resultado['avatar'] = $tweet->sender->profile_image_url;
-    $resultado['fecha'] = $tweet->created_at;
+  }
 
-    $general[$i] = $resultado;
-    $i++;
+
+  if ($accion == 'enviaMensaje') {
+      
+    $nombre = $_REQUEST['nombre'];
+    $mensaje = $_REQUEST['mensaje'];
+    $twitter = $connection->post('direct_messages/new', array('screen_name' => $nombre,'text' => $mensaje));
+    
+    echo json_encode($twitter);
 
   }
 
-  echo json_encode($general);
+  if ($accion == 'tweet') {
+      
+      $noticia = $_REQUEST['noticia'];
+      $twitter = $connection->post('statuses/update', array('status' => $noticia));
+
+      echo json_encode($twitter);
+  }
+
+
+  if ($accion == 'seguidores') {
+      
+    $twitter = $connection->get('followers/list');
+    $i = 0;
+
+      foreach ($twitter->users as $user) {
+        
+        $resultado['usuario'] = $user->screen_name;
+        $resultado['nombre'] = $user->name;
+        $resultado['avatar'] = $user->profile_image_url;
+        
+        $general[$i] = $resultado;
+        $i++;
+
+      }
+
+      echo json_encode($general);
+
+  }
+  
 
 ?>
