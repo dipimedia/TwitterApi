5ad2ca8ec3a629e7319de24520adedfebee9262e
Sergio Alcala <checo_2k2@hotmail.com>
2014-01-08 18:01:47 -0600

    Anexo de Archivos y preparando para Angular

--- js/main.js
+++ js/main.js
@@ -7,18 +7,79 @@ function muestraMensjaes(){
 						'maxCharacterSize': 140,
 						'originalStyle': 'originalTextareaInfo',
 						'warningStyle' : 'warningTextareaInfo',
-						'warningNumber': 130,
+						'warningNumber': 100,
 						'displayFormat' : '#input/#max'
 				};
-	$('textarea').textareaCount(options);
 
+	$('#noticia').textareaCount(options);
+	$('#mensaje').textareaCount(options);
 	$('#twettea').on('click',enviatweet);
-
 	$('#enviaMensaje').on('click',enviaMensaje);
-
 	$('#seguidores').on('click',muestraSeguidores);
 
+	$('#respuesta').find('#resp').on('click',function () {
+		alert('hola');
+	});	
 
+}
+
+function respondeMensaje(e){
+
+	 e.preventDefault()
+  	$('#dm').tab('show')
+
+	var usuario = $('#user').val();
+	console.log(usuario);
+
+}
+
+function ObtenMensajes(){
+
+var home = $('#respuesta');    
+      
+	home.html('');
+
+	var barra = '';
+	barra += '<div class="progress progress-striped active">';
+	barra += '<div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"><span class="sr-only">100% Complete</span></div>';
+	barra += '</div>';
+
+	home.html( barra );
+
+	$.get('api.php?funcion=mensajes',function (data){
+
+		var datos = $.parseJSON(data);
+
+		//console.log(datos);
+		home.html('');
+
+		var pagina;
+
+		if (datos.length > 0 ){
+
+			$.each(datos, function(index, valor) {
+
+			    pagina = '';
+				pagina += '<a href="#" class="list-group-item">';
+				pagina += '<img src="' + valor.avatar + '" alt="@"' + valor.de + ' class="img-circle">';
+				pagina += '<h4 class="list-group-item-heading">@<span class="user">' + valor.de + '</span></h4>';
+				pagina += '<p class="list-group-item-text">' + valor.texto + '</p>';
+				pagina += '<button type="button" id="resp" class="btn btn-primary">Responder</button></a>';
+
+				home.append(pagina);
+			});
+
+		}else{
+
+		    pagina = '';
+			pagina += '<h4 class="list-group-item-heading">No se Encontro Ningun Mensaje</h4>';
+			home.append(pagina);
+		}
+
+	});
+
+		
+	
 }
 
 function muestraSeguidores () {
@@ -48,9 +109,9 @@ function muestraSeguidores () {
 			$.each(datos, function(index, valor) {
 
 			    pagina = '';
-				pagina += '<a href="' + valor.usuario + '" class="list-group-item">';
+				pagina += '<a href="#" class="list-group-item">';
 				pagina += '<img src="' + valor.avatar + '" alt="@"' + valor.usuario + ' class="img-circle">';
-				pagina += '<h4 class="list-group-item-heading">@' + valor.usuario + '</h4>';
+				pagina += '<h4 class="list-group-item-heading">@<span class="user">' + valor.usuario + '</span></h4>';
 				pagina += '<p class="list-group-item-text">' + valor.nombre + '</p></a>';
 				
 				home.append(pagina);
@@ -116,50 +177,6 @@ function cuentaLetras(e){
 	
 }
 
-function ObtenMensajes(){
 
-var home = $('#respuesta');    
-      
-	home.html('');
-
-	var barra = '';
-	barra += '<div class="progress progress-striped active">';
-	barra += '<div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"><span class="sr-only">100% Complete</span></div>';
-	barra += '</div>';
-
-	home.html( barra );
-
-	$.get('api.php?funcion=mensajes',function (data){
-
-		var datos = $.parseJSON(data);
-
-		//console.log(datos);
-		home.html('');
-
-		var pagina;
-
-		if (datos.length > 0 ){
-
-			$.each(datos, function(index, valor) {
-
-			    pagina = '';
-				pagina += '<a href="' + valor.id + '" class="list-group-item">';
-				pagina += '<img src="' + valor.avatar + '" alt="@"' + valor.de + ' class="img-circle">';
-				pagina += '<h4 class="list-group-item-heading">@' + valor.de + '</h4>';
-				pagina += '<p class="list-group-item-text">' + valor.texto + '</p></a>';
-				
-				home.append(pagina);
-			});
-
-		}else{
-
-		    pagina = '';
-			pagina += '<h4 class="list-group-item-heading">No se Encontro Ningun Mensaje</h4>';
-			home.append(pagina);
-		}
-
-	});			
-	
-}
 
 $(document).on('ready', muestraMensjaes);
